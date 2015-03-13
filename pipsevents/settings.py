b3d9afc62bc8e5eeea66aa0bf83ed67412d8a43b
Becky Smith <becky.smith@hogarthww.com>
2015-03-13 16:21:20 +0000

    add mailcatcher

--- pipsevents/settings.py
+++ pipsevents/settings.py
@@ -222,3 +222,11 @@ SUIT_CONFIG = {
 }
 
 INTERNAL_IPS = '127.0.0.1'
+
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = '127.0.0.1'
+    EMAIL_HOST_USER = ''
+    EMAIL_HOST_PASSWORD = ''
+    EMAIL_PORT = 1025
+    EMAIL_USE_TLS = False
